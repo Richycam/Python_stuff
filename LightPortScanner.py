f95fde9aecd55e183f8404897c0e1c01ccfbea98
Richycam <97134605+Richycam@users.noreply.github.com>
2024-02-18 16:59:00 +0000

    Update LightPortScanner.py

--- LightPortScanner.py
+++ LightPortScanner.py
@@ -6,8 +6,8 @@ sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
 for port in range(100):
     try:
-        sock.bind((ip,port))
-        if sock.bind:
+        sock.connect_ex((ip,port))
+        if sock.connect_ex == 0:
             print('--OPEN-- Port open :',ip,port)
     except:
         print('--CLOSED-- Port Closed :',ip,port)
